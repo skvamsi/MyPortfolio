bc54ff108001f2e55be3f7bb7689b16fd591ad10
skvamsi <140873635+skvamsi@users.noreply.github.com>
2024-06-12 16:59:22 +0530

    Add files via upload

---
+++ call.js
@@ -0,0 +1,70 @@
+function showElement(inputDivToShow) {
+    var inputDiv1 = document.getElementById('id-1');  
+    var inputDiv2 = document.getElementById('id-2');
+    var inputDiv3 = document.getElementById('id-3');
+    var inputDiv4 = document.getElementById('id-4');
+    
+    // Hide all elements initially
+    inputDiv1.style.display = "none";
+    inputDiv2.style.display = "none";
+    inputDiv3.style.display = "none";
+    inputDiv4.style.display = "none";
+    
+    // Show the specified inputDiv
+    inputDivToShow.style.display = "block";
+}
+
+function call1() {
+    var inputDiv1 = document.getElementById('id-1');
+    showElement(inputDiv1);
+}
+
+function call2() {
+    var inputDiv2 = document.getElementById('id-2');
+    showElement(inputDiv2);
+}
+
+function call3() {
+    var inputDiv3 = document.getElementById('id-3');
+    showElement(inputDiv3);
+}
+
+function call4() {
+    var inputDiv4 = document.getElementById('id-4');
+    showElement(inputDiv4);
+}
+function showElement(inputDivToShow) {
+    var inputDiv1 = document.getElementById('id-1');  
+    var inputDiv2 = document.getElementById('id-2');
+    var inputDiv3 = document.getElementById('id-3');
+    var inputDiv4 = document.getElementById('id-4');
+    
+    // Hide all elements initially
+    inputDiv1.style.display = "none";
+    inputDiv2.style.display = "none";
+    inputDiv3.style.display = "none";
+    inputDiv4.style.display = "none";
+    
+    // Show the specified inputDiv
+    inputDivToShow.style.display = "block";
+}
+
+function call1() {
+    var inputDiv1 = document.getElementById('id-1');
+    showElement(inputDiv1);
+}
+
+function call2() {
+    var inputDiv2 = document.getElementById('id-2');
+    showElement(inputDiv2);
+}
+
+function call3() {
+    var inputDiv3 = document.getElementById('id-3');
+    showElement(inputDiv3);
+}
+
+function call4() {
+    var inputDiv4 = document.getElementById('id-4');
+    showElement(inputDiv4);
+}
